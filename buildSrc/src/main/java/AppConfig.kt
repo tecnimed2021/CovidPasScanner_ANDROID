6efb5d4f5b3830aecd99a39b3836191c21dd99b0
VLengineering <luca.valtellina@vlengineering.it>
2022-01-21 12:38:11 +0100

    Rilascio versione 1.40.32 Codice1

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -29,7 +29,7 @@ object Config {
     val javaVersion = JavaVersion.VERSION_1_8
 
     const val versionCode = 1
-    const val versionName = "1.40.31"
+    const val versionName = "1.40.32"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
