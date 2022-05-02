4d2623ecd2dadf2a78c68475bcbcb56dad0443df
Luca Valtellina <luca.valtellina@vlengineering.it>
2022-05-02 16:46:07 +0200

    Rilascio versione 1.42.17 Codice1

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -29,7 +29,7 @@ object Config {
     val javaVersion = JavaVersion.VERSION_1_8
 
     const val versionCode = 1
-    const val versionName = "1.42.16"
+    const val versionName = "1.42.17"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
