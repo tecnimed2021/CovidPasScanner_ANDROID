e1491c0242e11609c089868f76d3590a29d201bd
Luca Valtellina <luca.valtellina@vlengineering.it>
2022-05-04 15:29:50 +0200

    Rilascio versione 1.43.01 Codice1

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -29,7 +29,7 @@ object Config {
     val javaVersion = JavaVersion.VERSION_1_8
 
     const val versionCode = 1
-    const val versionName = "1.43.00"
+    const val versionName = "1.43.01"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
