4c3289d66ced99124f49cd8682d15f3fd6b888b0
Luca Valtellina <luca.valtellina@vlengineering.it>
2022-05-09 19:05:46 +0200

    Rilascio versione 1.43.09 Codice1

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -29,7 +29,7 @@ object Config {
     val javaVersion = JavaVersion.VERSION_1_8
 
     const val versionCode = 1
-    const val versionName = "1.43.08"
+    const val versionName = "1.43.09"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
     const val proguardConsumerRules = "consumer-rules.pro"
