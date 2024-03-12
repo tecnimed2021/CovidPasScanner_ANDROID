1cea06187f3ad2aae7b4f372b218d2f0576697b0
VLengineering <info@vlengineering.it>
2024-03-12 09:34:06 +0100

    AppSetup.
    Aggiunti parametri TempCorrection e TempCorrectionAir.

--- app/src/main/java/it/tecnimed/covidpasscanner/Tecnimed/AppSetup.java
+++ app/src/main/java/it/tecnimed/covidpasscanner/Tecnimed/AppSetup.java
@@ -4,12 +4,19 @@ import java.io.Serializable;
 
 public class AppSetup implements Serializable
 {
+    final public static int ORAL = 0;
+    final public static int RECTAL = 1;
+    final public static int AXILLA = 2;
+    final public static int CORE = 3;
     public static boolean SequenceTemperature = false;
     public static boolean SequenceGreenPass = false;
     public static boolean SequenceDocument = false;
 
     public static float RangeTempGreen = 37.5f;
     public static float RangeTempOrange = 0.5f;
+
+    public static int TempCorrection = ORAL;
+    public static boolean TempCorrectionAir = false;
 
     public boolean getSequenceTemperature()
     {
@@ -50,5 +57,21 @@ public class AppSetup implements Serializable
     public void setRangeOrange(float t)
     {
         RangeTempOrange = t;
+    }
+    public int getCorrection()
+    {
+        return TempCorrection;
+    }
+    public void setCorrection(int t)
+    {
+        TempCorrection = t;
+    }
+    public boolean getCorrectionAir()
+    {
+        return TempCorrectionAir;
+    }
+    public void setCorrectionAir(boolean t)
+    {
+        TempCorrectionAir = t;
     }
 }
